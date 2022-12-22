9e238445917306ae57208188a63bd1d6e164c41c
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-12-22 02:59:25 +0000

    Hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./componets/header/Header";
+import Hero from "./componets/hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
